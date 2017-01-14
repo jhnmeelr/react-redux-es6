79871d254c12f313a6f8c93d0fccc9354b332b2f
jhnmeelr <jhn.meelr@gmail.com>
2017-01-14 18:33:17 +0200

    Initial App Structure

---
+++ src/components/entries/EntriesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class EntriesPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Entries</h1>
+            </div>
+        );
+    }
+}
